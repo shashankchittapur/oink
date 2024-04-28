704d66be58844e054fd57fb65d05575dee2e2112
Shashank Chittapur <shashank.chittapur@solibri.com>
2024-04-28 10:41:24 +0530

    Remove theme selector from header and make header height 24

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,18 +1,12 @@
 import { MainNav } from "./main-nav";
 import { MobileNav } from "./mobile-nav";
-import { ModeToggle } from "./mode-toggle";
 
 export default function SiteHeader() {
     return (
-        <header className="supports-backdrop-blur:bg-background/60 sticky h-16 top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
-            <div className="container flex h-full items-center">
+        <header className="flex supports-backdrop-blur:bg-background/60 sticky h-24 top-0 z-50 w-full border-b bg-background/95 backdrop-blur items-center">
+            <div className="flex h-full items-center">
                 <MainNav />
                 <MobileNav />
-                <div className="flex flex-auto items-end justify-end space-x-5 lg:pr-20">
-                    <nav className="flex items-center space-x-5">
-                        <ModeToggle />
-                    </nav>
-                </div>
             </div>
         </header>
     )
